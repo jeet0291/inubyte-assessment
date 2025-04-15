d7a9f70f543da0cad06f7395e95a59afbd3a3ef1
Jitendra Chandora <jeet.io.0291@gmail.com>
2025-04-15 17:02:23 +0530

    Added 4 use cases for negative number

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 function add(str){
+    // negative numbers not allowed <negative_number>
+    const errors = [];
     let total = 0;
-    for(let i=0;i<str.length;i++){
-        let char = str[i];
-        if(!isNaN(char)){
-            total += Number(char);
+    try {
+        for(let i=0;i<str.length;i++){
+            
+            if(!isNaN(str[i])){
+                if( typeof str[i-1] != 'undefined' &&  str[i-1] == '-'){
+                    errors.push(`negative numbers not allowed -${str[i]}`)
+                }
+                total += Number(str[i]);
+            }
+        }
+        if (errors.length > 0) {
+            throw new Error(errors.join(', '));
+        }
+        return total;
+    }catch(err){
+        if(err){
+            console.log(err);
         }
     }
-    return total;
 }
 
 
@@ -14,9 +28,12 @@ function add(str){
 // Use Case 1 //
 let UC1 = add('8888888888');
 console.log('Total: ', UC1)
-// Use Case 2 //
+// // Use Case 2 //
 let UC2 = add('1\n2,3');
 console.log('Total: ', UC2)
-// Use Case 3 //
+// // Use Case 3 //
 let UC3 = add('//;\n1;2');
+console.log('Total: ', UC3)
+// Use Case 4 //
+let UC4 = add('1111-81111-9');
 console.log('Total: ', UC3)
