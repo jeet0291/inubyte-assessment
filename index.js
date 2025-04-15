b9be83fc28d82e75d603627db9537a7d99258cfe
Jitendra Chandora <jeet.io.0291@gmail.com>
2025-04-15 16:41:54 +0530

    Added 3 use cases

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ function add(str){
     for(let i=0;i<str.length;i++){
         let char = str[i];
         if(!isNaN(char)){
-            console.log(char);
             total += Number(char);
         }
     }
@@ -11,5 +10,13 @@ function add(str){
 }
 
 
-let total = add('12//3ksjh\nfksj;h32');
-console.log('##', total)
+
+// Use Case 1 //
+let UC1 = add('8888888888');
+console.log('Total: ', UC1)
+// Use Case 2 //
+let UC2 = add('1\n2,3');
+console.log('Total: ', UC2)
+// Use Case 3 //
+let UC3 = add('//;\n1;2');
+console.log('Total: ', UC3)
